5b2c91e6220e84581a0e01dbd5aa0f5e21792d41
Ruslan I. Hagverdi <RuslanIh@CODE.LOCAL>
2018-12-16 19:36:53 +0400

    hjh

---
+++ 16.12.2018/cbar/cbar/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace cbar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UpdateDb = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UpdateDb
+            // 
+            this.UpdateDb.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateDb.Location = new System.Drawing.Point(444, 226);
+            this.UpdateDb.Name = "UpdateDb";
+            this.UpdateDb.Size = new System.Drawing.Size(99, 32);
+            this.UpdateDb.TabIndex = 0;
+            this.UpdateDb.Text = "UpdateDb";
+            this.UpdateDb.UseVisualStyleBackColor = true;
+            this.UpdateDb.Click += new System.EventHandler(this.UpdateDb_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(237, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(422, 38);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 40);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(35, 167);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(266, 53);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Netice";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 270);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.UpdateDb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UpdateDb;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 16.12.2018/cbar/cbar/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace cbar
+{
+    public partial class Form1 : Form
+    {
+        public string queryString;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateDb_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=192.168.19.35\SQLEXPRESS;Initial Catalog=ManatDbR;Persist Security Info=True;User ID=sa;Password=123456";
+
+
+            
+
+            var webClient = new WebClient();
+            var data = webClient.DownloadData("https://www.cbar.az/currencies/14.12.2018.xml");
+            var xmlData = Encoding.UTF8.GetString(data);
+
+            StringReader xmlStreamData = new StringReader(xmlData);
+
+            XmlSerializer xmlser = new XmlSerializer(typeof(XmlRegust.ValCurs));
+            var myValCurs = (XmlRegust.ValCurs)xmlser.Deserialize(xmlStreamData);
+            //foreach(var el in myValCurs.ValType)
+           // {
+                foreach(var elm in myValCurs.ValType[1].Valute)
+                {
+                    queryString += $"INSERT INTO [Table](Code,Nominal,Name,Value,ValueType,ValDateTime)" +
+               $"VALUES ('{elm.Code}',{elm.Nominal},N'{elm.Name}',{elm.Value},N'{ myValCurs.ValType[1].Type}','{myValCurs.Date}')";
+                }
+//}
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+
+            }
+        }   
+    }
+}
